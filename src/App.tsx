0f0ea3a6b6e5d19fb87efe927e07f472436fc088
Madusha Prasanjith <hi@madusha.me>
2022-03-28 10:58:51 +0530

    Add basic UI styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import NavBar from "./components/Navbar";
+
 export function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+
+      <div className="hero grow bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Web3-React Integration</h1>
+            <p className="py-6">
+              This is a sample integration for the Web3-react library. To get
+              started, sign-in using your wallet.
+            </p>
+            <button className="btn btn-primary">Sign-in with wallet</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
